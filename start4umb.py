1a1b18135aae1bc4ccba5c72408f4f2d94dd87c9
Jeremy Lapierre <jlapier@gwdu103.global.gwdg.cluster>
2019-09-26 11:51:29 +0200

    Change for new parser in start4umb.py

--- start4umb.py
+++ start4umb.py
@@ -1,43 +1,19 @@
-#!/usr/users/jlapier/.conda/envs/env1/bin/python
+#!/usr/bin/env python
 
 import sys
 import numpy as np
 import pandas as pd
+import plumed_pandas
 
 def main():
     '''
-    parsing the colvar.txt, extracting RMSDs and angles data, find the closest nCV value to target_val
+    parsing the colvar.txt, find the closest nCV value to target_val
     '''
-    
-    time = []
-    nCV = []
-    
-    #read file and extract RMSDs and angle data
-    with open(sys.argv[1]) as f:
-        fl = f.readline()
-        fls=fl.split(' ')
-        fls = [x.replace("\n", "") for x in fls]
-        fls=[x.replace('.', '_') for x in fls]
-        fls=fls[2:]
-        #print(fls)
-        timeid=fls.index('time')
-        nCVid=fls.index('nCV')
-        #print(timeid, nCVid)
-        for line in f:
-            t=line.split()
-            if t[0] == '#!':
-                next(f)
-            else:
-                time.append(round(float(t[timeid]), 2))
-                nCV.append(float(t[nCVid]))
-    
+
+    #read COLVAR.txt
+    df=plumed_pandas.read_as_pandas(sys.argv[1])
+
     #extract time step of frame closest to the target_value
-    time=pd.DataFrame(time)
-    nCV=pd.DataFrame(nCV)
-    time.columns = ['time']
-    nCV.columns = ['nCV']
-    df=time.join(nCV)
-    #neareast = df.iloc[(df['nCV']-float(sys.argv[2])).abs().idxmin()]
     value=df.iloc[(df['nCV']-float(sys.argv[2])).abs().idxmin()]['time']
     return value
 
