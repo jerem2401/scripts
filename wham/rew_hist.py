19d02ee3cc23afb4c7061e4d547a015b9098a054
Jeremy Lapierre <jeremy.lapierre@uni-saarland.de>
2019-10-10 17:21:19 +0200

    minor change

--- wham/rew_hist.py
+++ wham/rew_hist.py
@@ -4,34 +4,27 @@ import plumed_pandas
 import numpy as np
 import pandas as pd 
 import argparse
+import re
 
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', help='give colvar_file', nargs='1', action='store', dest='f')        
+    parser.add_argument('-f', help='give colvar_file, for building weithed histo', action='store', dest='f')        
+    args = parser.parse_args()
 
     df=plumed_pandas.read_as_pandas(args.f)
-    df = df[['nCV','guide_restraint.bias']]
+    df = df[['nCV','nCV_restraint.bias']]
+    
+    hist2, bin_edges = np.histogram(df['nCV'], bins=200, weights=[np.exp(i/2.49434) for i in df['nCV_restraint.bias']])
+    
+    d = {'z': bin_edges[0:-1],'hist': hist2}
+    
+    hdf=pd.DataFrame(d)
 
-    bstep=(df['nCV'].max()-df['nCV'].min())/200
-    print(df['nCV'].max(), df['nCV'].min())
-    print(bstep)
-    print(round(bstep,6))
-    bval=[]
-    for i in range(1,201):
-        bval.append(round(df['nCV'].min()+bstep*i,6))
-    print(bval)
-    hist=pd.DataFrame(bval)
-    hist.columns = ['bval']
-    hist['counts'] = 0
-    for c, (i, j) in enumerate(zip(df['nCV'], df['guide_restraint.bias'])):
-        if c in [i for i in range(1000,500000,1000)]:
-            print(c)
-        for index, row in hist.iterrows():
-            if index == 0:
-                N=df['nCV'].min()
-            if N <= i < row['bval']:
-                hist.loc[index,'counts'] = hist.loc[index,'counts'] + np.exp(j/2.5)
-            N=row['bval']
-    #print(hist.loc[0,'counts'])
-    display(hist)
+    pos=re.search('(-|[0-9]|\.)+(?=.txt)', args.f).group()    
+
+    np.savetxt(r'./histo_'+str(pos)+'_.dat', hdf.values, header="col1=z col2=hist\n#1 #2 "+str(pos)+"\n#1 #2 5500", fmt='%.6f')
+
+# If called from the command line... 
+if __name__ == "__main__":
+    main()
