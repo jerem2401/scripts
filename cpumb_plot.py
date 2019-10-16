c3e60f753a52816de618f29f284990a515a7e544
Jeremy Lapierre <jeremy.lapierre@uni-saarland.de>
2019-10-16 13:00:21 +0200

    updated cpumbplot for running with new umb settings, uses the better parser as well

--- cpumb_plot.py
+++ cpumb_plot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 import glob
-from readplumed import *
+import plumed_pandas
 import sys
 import argparse
 
@@ -16,7 +16,7 @@ def main():
     parser.add_argument('-f', help='give colvar_files with spaces inbetween, should not be used with -umb', nargs='+', action='store', dest='f')
     parser.add_argument('-umb', help='specify that umb plot mode should be turn on', action='store_true', dest='umb')
     parser.add_argument('--a', help='provide alpha if not in directory_name under certain format', action='store', dest='a', type=float)
-    parser.add_argument('--o', help='give path for plot output', action='store', dest='o')
+    parser.add_argument('-o', help='give path for plot output', action='store', dest='o')
     args = parser.parse_args()
 
 
@@ -58,25 +58,20 @@ def main():
         eqCVval=nCV(Xeq,Yeq)
         axCVval=nCV(Xax,Yax)
         #################################################################################################
-
-        RMSDeq = []
-        RMSDax = []
-        phi = []
-        psi = []
             
         fig=plt.figure(1, figsize=(10,6*len(files)))
         fig=plt.figure(1)
 
         for pltn, i in zip(range(1, len(files)*2+1, 2), files):
-
-            RMSDeq = rdata(i)[0]
+            df=plumed_pandas.read_as_pandas(i)
+            RMSDeq = df['RMSDEQ']
             idd=[elem for elem in range(0, len(RMSDeq),1)]
             RMSDeq = [RMSDeq[i] for i in idd]
-            RMSDax = rdata(i)[1]
+            RMSDax = df['RMSDAX']
             RMSDax = [RMSDax[i] for i in idd]
-            phi = rdata(i)[2]
+            phi = df['phi']
             phi = [phi[i] for i in idd]
-            psi = rdata(i)[3]
+            psi = df['psi']
             psi = [psi[i] for i in idd]
             
             plt.subplot(len(files), 2, pltn)
@@ -123,40 +118,34 @@ def main():
             '''
             )
             #path of umb working dir with all subdir called "E_*" for each um windows with diff ksi values
-            #(out,err) = subprocess.Popen(['pwd'],stdout=subprocess.PIPE).communicate()
-            #path=out.decode().strip()+'/' 
-            #path=out.decode().strip().replace('analysis','')+"setup/"
-            path=str(sys.argv[1])
+            #path=str(sys.argv[1])
 
             #list of all colvar file outputs from plumed, format: "colvar.*"
-            #colvar_list=[os.path.join(os.path.join(path, elem), x)
-            #            for elem in os.listdir(path)
-            #            if os.path.isdir(os.path.join(path, elem)) == True
-            #            if "E_" in elem
-            #            for x in os.listdir(os.path.join(path, elem))
-            #            if "colvar" in x]
-            colvar_list=[os.path.join(f, x)
-                    for f in glob.glob(path + "**/", recursive=True)
-                    for x in os.listdir(f)
-                    if re.search(r"colvar_([0-9]|-|\.)", x) !=None]
+            #colvar_list=[os.path.join(f, x)
+            #        for f in glob.glob(path + "*/", recursive=True) for all directories in path
+            #        for x in os.listdir(f) for all the elements in directories
+            #        if re.search(r"colvar_([0-9]|-|\.)", x) !=None]
+
+            colvar_list=os.popen('ls E*/colvar* | sort -t _ -k 2 -n').read().split()
 
             #list of ksi values from which we've restrained the coordinates, take this values from umbsetup.sh ?
-            #CVval=[elem.split('_')[1] for elem in os.listdir(path) if os.path.isdir(os.path.join(path, elem)) == True if "E_" in elem]
-            CVval=[elem.split('_')[-1].strip("/") for elem in glob.glob(path + "**/", recursive=True) if "E_" in elem]
+            #CVval=[elem.split('_')[-1].strip("/") for elem in glob.glob(path + "**/", recursive=True) if "E_" in elem]
+            
+            CVval=[re.search('(?<=E_).*(?=/)',i).group() for i in colvar_list]
 
             #sorting by inscreasing order for "levels" in plotting and for sorting colvar files in colvar_list
-            CVval2=sorted([float(i) for i in CVval])
+            #CVval2=sorted([float(i) for i in CVval])
 
+            #following paragraph not needed anymore since now os.popen + sort already list by increasing value of ksi
             #get dictionary of CVval values with their indexes (last line inverse keys and values)
-            indexing = enumerate(CVval2) 
-            dico=dict(list(indexing))
-            dico={v: k for k, v in dico.items()}
-
+            #indexing = enumerate(CVval2) 
+            #dico=dict(list(indexing))
+            #dico={v: k for k, v in dico.items()}
             #reordering values in list according to CVval2 order
-            colvar_listS = [None] * len(colvar_list)
-            for i in colvar_list:
-                colvar_listS[dico[float(re.search(r"(?<=E_).*(?=/)", i).group())]] = i
-            colvar_list=colvar_listS
+            #colvar_listS = [None] * len(colvar_list)
+            #for i in colvar_list:
+            #    colvar_listS[dico[float(re.search(r"(?<=E_).*(?=/)", i).group())]] = i
+            #colvar_list=colvar_listS
 
             #cut colvar_list in several lists of 20 elem to lighten the plot generation and avoid crash (for std desktop ressources)
             clcut=[colvar_list[i:i + 20] for i in range(0, len(colvar_list), 20)]
@@ -164,50 +153,58 @@ def main():
             #function parameters#######################CHANGE CODE TO GET PARAM FROM COLVAR##########################
             a=0.5
             deq=0.05
-            x = np.arange(0, 0.2, 0.005)
-            y = np.arange(0, 0.2, 0.005)
+            x = np.arange(0.001, 0.180, 0.005)
+            y = np.arange(0.001, 0.180, 0.005)
             X, Y = np.meshgrid(x, y)
             Z = ((deq/X)**a)-((deq/Y)**a)
             #function parameters#######################CHANGE CODE TO GET PARAM FROM COLVAR##########################
 
             #data objects
-            RMSDeq = []
-            RMSDax = []
-            phi = []
-            psi = []
-
             for nbl, k in enumerate(clcut, 0):
                 plt.figure(1)
                 plt.figure(figsize=(12,5*len(k))) 
                 for pltn, i in zip(range(1, len(k)*2+1, 2), k):
-
-                    RMSDeq = rdata(i)[0]
-                    RMSDax = rdata(i)[1]
-                    phi = rdata(i)[2]
-                    psi = rdata(i)[3]
+                    df=plumed_pandas.read_as_pandas(i)
+                    RMSDeq = df['RMSDEQ'][::2]
+                    RMSDax = df['RMSDAX'][::2]
+                    phi = df['phi'][::2]
+                    psi = df['psi'][::2]
 
                     #get param of restraints from the plumed input files, todo: build a dic {colvar_file : CVval} instead of the wierd trick in the following line (should change also in l.87-93)
-                    with open(re.search(r".*E_([0-9]|-|\.)*", i).group()+"/plumed_"+format(CVval2[k.index(i)+nbl*20], '.2f')+".dat", "r") as plume:
+                    with open(re.search(r".*E_([0-9]|-|\.)*", i).group()+"/plumed_"+CVval[k.index(i)+nbl*20]+".dat", "r") as plume:
+                        gonextl1=False
+                        gonextl2=False
                         for line in plume:
-                            if "KAPPA" in line:
+                            if gonextl1:
+                                #print(line)
                                 param = re.search(r"AT.*KAPPA[^ ]*", line)
+                                gonextl1=False
+                            if gonextl2:
+                                #print(line)
+                                param2 = re.search(r"AT.*KAPPA[^ ]*", line)
+                                gonextl2=False
+                            if "# nCV restraint" in line:
+                                gonextl1=True
+                                #print(line)
+                            if "# guideCV" in line:
+                                gonextl2=True
                     plume.close()
 
                     plt.subplot(len(k), 2, pltn)
                     levels=[-1.0, -0.5, -0.2, -0.1, 0, 0.1, 0.2, 0.5, 1.0]
-                    if CVval2[k.index(i)+nbl*20] in levels:
-                        levels.remove(CVval2[k.index(i)+nbl*20])
+                    if float(CVval[k.index(i)+nbl*20]) in levels:
+                        levels.remove(float(CVval[k.index(i)+nbl*20]))
                         CS = plt.contour(X, Y, Z, levels=levels)
-                        CS2 = plt.contour(CS, levels=[CVval2[k.index(i)+nbl*20]], colors='red', linestyles='dashed')
+                        CS2 = plt.contour(CS, levels=[float(CVval[k.index(i)+nbl*20])], colors='red', linestyles='dashed')
                     else:
                         CS = plt.contour(X, Y, Z, levels=levels)
-                        CS2 = plt.contour(CS, levels=[CVval2[k.index(i)+nbl*20]], colors='red', linestyles='dashed')
+                        CS2 = plt.contour(CS, levels=[float(CVval[k.index(i)+nbl*20])], colors='red', linestyles='dashed')
                     plt.clabel(CS, inline=1, fontsize=10)
                     plt.clabel(CS2, inline=1)
                     plt.xlabel('RMSDeq')
                     plt.ylabel('RMSDax')
                     plt.scatter(RMSDeq, RMSDax, c=list(range(1, len(RMSDax)+1, 1)), cmap=plt.cm.get_cmap('rainbow'), s=10)
-                    plt.title(param.group())
+                    plt.title(param.group()+'\n'+param2.group())
 
                     plt.subplot(len(k), 2, pltn+1)
                     plt.xlim(-3.2, 3.2)
@@ -219,7 +216,10 @@ def main():
 
 
                 plt.tight_layout()
-                plt.savefig('plot'+str(nbl)+'.jpeg')
+                if args.o == None:
+                    plt.savefig('plot'+str(nbl)+'.jpeg', quality=40)
+                else:
+                    plt.savefig(args.o+'/plot'+str(nbl)+'.jpeg', quality=30)
                 plt.close()
 
         else:
