20601ea329a52365c996fb69266fa69dfb50aec9
Jeremy Lapierre <jeremy.lapierre@uni-saarland.de>
2019-10-23 12:46:24 +0200

    adding wham scripts (python 2 and 3)

---
+++ wham/do_whampy2.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os, sys, re, math
+
+
+def do_wham(z, histos, pos, kappa, RT = 2.49434, tol = 1e-6, bCycl = False,
+                zmin = float('nan'), zmax = float('nan')):
+
+    maxchangeZ = 1
+    nHist      = len(histos)
+    nz_input   = len(z)
+    dz         = z[1] - z[0]
+
+    # if (bCycl):
+    #     if (not (cycl_max > cycl_min)):
+    #         print 'Error, cycl_max must be larger than cycl_min'
+    #         sys.exit(0)
+
+    #     icycl_min = int(round((cycl_min-z[0])/dz))
+    #     icycl_max = int(round((cycl_max-z[0])/dz))
+
+    #     if (icycl_min < 0):
+    #         icycl_min = 0
+    #     if (icycl_max > nz_input-1):
+    #         icycl_max = nz_input-1
+
+        # Map histograms over periodic boundary
+        
+            
+        # # Find boundaries
+        # bound_min = 1e20
+        # bound_max = -1e20
+        # j_min     = 10000
+        # j_max     = -1
+        # for i in range(nHist):
+        #     for j in range(nz_input):
+        #         if histos[i][j] > 0:
+        #             j_min = min(j_min, j)
+        #             j_max = max(j_max, j)
+        # print 'Determined boundaries:'
+        # print '\tLower bound = %10g  (index %d)' % (z[j_min], j_min)
+        # print '\tUpper bound = %10g  (index %d)' % (z[j_max], j_max)
+    
+    # Bounds should only be given with bCycl
+    if (bCycl):
+        if (math.isnan(zmin) or math.isnan(zmax)):
+            print 'Error, with bcycl, you must provide zmin and zmax'
+    else:
+        if (not math.isnan(zmin) or not math.isnan(zmax)):
+            print 'Error, provide zmin and zmax only with bCycl'
+        zmin = z[0]
+        zmax = z[-1]
+            
+    izmin = int(round((zmin-z[0])/dz))
+    izmax = int(round((zmax-z[0])/dz))
+    if (izmin < 0):
+        izmin = 0
+    if (izmax > nz_input-1):
+        izmax = nz_input-1
+
+    nzused = izmax-izmin+1
+
+    zused = z[izmin:izmin+nzused]
+    
+    print '\nDoing WHAM:'
+    print '\t# of histograms = %d' % nHist
+    print '\t# of bins given = %d' % nz_input
+    print '\t# of bins used  = %d' % nzused
+    print '\tz range given   = %g to %g' % (z    [0], z    [-1]+dz)
+    print '\tz range used    = %g to %g' % (zused[0], zused[-1]+dz)
+    print '\tTolerance       = %g' % tol
+
+    histos_used = np.zeros([nHist, nzused])
+    
+    if (bCycl == False):
+        histos_used = histos
+    else:
+        for i in range(nHist):
+            nBinsOutside_lower = izmin
+            nBinsOutside_upper = nz_input - izmax
+            # Map histos outside boundaryies [izmin:izmax] back into the [izmin:izmax]
+            histos_used[i]                                    = histos[i][izmin:izmin+nzused]
+            if nBinsOutside_upper > 0:
+                histos_used[i][nzused-nBinsOutside_lower:] += histos[i][0:nBinsOutside_lower]
+            if nBinsOutside_upper:
+                histos_used[i][0:nBinsOutside_upper]         += histos[i][nz_input-nBinsOutside_upper:]
+    
+    expZ         = np.ones(nHist)   # = e^Z[i]
+    nPointsHist  = np.zeros(nHist)
+    nPoints_times_exp_minus_U_over_RT = []
+    denom        = np.zeros(nzused)
+    numer        = np.zeros(nzused)
+    exp_minus_U_over_RT = []              # exp( -V[umb]/RT)
+
+    # Write all histos into one xvg file
+    if (bCycl):
+        fp = open('histo_1D_cycl.xvg', 'w')
+        for i in range(len(zused)):
+            print >> fp, '%10g ' % (zused[i]),
+            for w in range(nHist):
+                print >> fp, ' %10g' % (histos_used[w][i]),
+            print >> fp
+        fp.close()
+
+    
+    # Store e^(-U/RT) and Npoints * e^(-U/RT) for all histos, so
+    # to avoid that we need to compute a lot of exp() in the loop
+    for i in range(nHist):
+        nPointsHist[i] = np.sum(histos_used[i])
+
+        if (bCycl == False):
+            sqr_distance = ((zused+dz/2) - pos[i])**2
+        else:
+            # Get shortest distance, possibly over the periodic boundaries
+            L             = zused[-1] - zused[0] + dz
+            deltaz        = (zused+dz/2) - pos[i]
+            sqr_distance1 = (deltaz   )**2
+            sqr_distance2 = (deltaz + L)**2
+            sqr_distance3 = (deltaz - L)**2
+            sqr_distance  = np.min([sqr_distance1, sqr_distance2, sqr_distance3], axis=0)            
+        
+        expU            = np.exp(-0.5 * kappa[i] * sqr_distance / RT)
+        exp_minus_U_over_RT.append( expU )
+        nPoints_times_exp_minus_U_over_RT.append( nPointsHist[i] * expU)
+    
+    # The main WHAM loop
+    N = 1
+    while (maxchangeZ > tol):
+
+        bCheckChange = ((N % 100) == 0)
+        if (bCheckChange):
+            Zold = np.log(expZ)
+
+        # Compute profile
+        # E.g.: Roux, Comp Phys Comm, 91, 275-282 (1995), eq. 8
+        numer.fill(0)
+        denom.fill(0)
+        for i in range(nHist):
+            numer += histos_used[i]
+            denom += nPoints_times_exp_minus_U_over_RT[i] * expZ[i]
+
+        rho = numer / denom
+
+        # Update Z
+        for i in range(nHist):
+            expZ[i] = 1.0 / (np.sum(exp_minus_U_over_RT[i] * rho))
+            
+        # Get change in Z
+        if (bCheckChange):
+            Znew       = np.log(expZ)
+            maxchangeZ = (np.fabs(np.log(expZ) - Zold)).max()
+            print 'WHAM iteration %5d, maxchange of Z\'s: %g' % (N, maxchangeZ)
+            
+        N += 1
+    
+    return zused, rho, np.log(expZ)
+    
+
+
---
+++ wham/wham_pmf.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+import numpy as np
+import os, sys, re, math
+from do_wham import do_wham
+import matplotlib.pyplot as plt
+import matplotlib
+
+RT = 2.49434
+
+histo_files = os.popen('ls histo* | sort -t _ -k 2 -n').read().split()
+N = len(histo_files)
+
+hist  = []
+pos   = np.zeros(N)
+kappa = np.zeros(N)
+
+
+n = 0
+for h in histo_files:
+
+    print('Reading %s' % h)
+    fp = open(h, 'r')
+
+    l      = fp.readline()
+    l      = fp.readline()
+    pos1   = float(l.split()[3])
+    l      = fp.readline()    
+    kappa1 = float(l.split()[3])
+    fp.close()
+
+    z, thishist = np.loadtxt(h, unpack=True)
+    hist.append( thishist )
+    
+    print ('\tpos = %10g  kappa = %10g   %d bins' % (pos1, kappa1, len(z)))
+
+    pos  [n] = pos1
+    kappa[n] = kappa1
+    n       += 1
+
+    
+
+zused, rho, Z = do_wham(z, hist, pos, kappa, RT = 2.49434, tol = 1e-6, bCycl = False)
+
+#The main WHAM loop
+#    N = 1
+#    while (maxchangeZ > tol):
+#
+#        bCheckChange = ((N % 100) == 0)
+#        if (bCheckChange):
+#            Zold = np.log(expZ)
+#
+#        # Compute profile
+#        # E.g.: Roux, Comp Phys Comm, 91, 275-282 (1995), eq. 8
+#        numer.fill(0)
+#        denom.fill(0)
+#        for i in range(nHist):
+#            numer += histos_used[i]
+#            denom += nPoints_times_exp_minus_U_over_RT[i] * expZ[i]
+#
+#        rho = numer / denom
+#
+#        # Update Z
+#        for i in range(nHist):
+#            expZ[i] = 1.0 / (np.sum(exp_minus_U_over_RT[i] * rho))
+#            
+#        # Get change in Z
+#        if (bCheckChange):
+#            Znew       = np.log(expZ)
+#            maxchangeZ = (np.fabs(np.log(expZ) - Zold)).max()
+#            print('WHAM iteration %5d, maxchange of Z\'s: %g' % (N, maxchangeZ))
+#            
+#        N += 1
+#    
+#    return zused, rho, np.log(expZ)
+
+
+pmf = np.zeros_like(rho)
+for i in range(len(rho)):
+    if (rho[i] > 0):
+        pmf[i] = -RT * np.log(rho[i])
+    else:
+        pmf[i] = 0
+
+fp = open('test_wham_pmf.out', 'w')
+for i in range(len(zused)):
+    print(str(zused[i])+'    '+str(pmf[i]), file=fp)
+    #print("{:.3f}    {:.3f}".format(zused[i],pmf[i]), file=fp) #To do: adding format to remove digits
+fp.close()
+
+fp = open('test_wham_z.out', 'w')
+for i in range(N):
+    print(str(pos[i])+'    '+str(Z[i]), file=fp)
+fp.close()
+
+#plot PMF
+
+matplotlib.rcParams.update({'font.size': 15})
+
+x, y = np.loadtxt('test_wham_pmf.out', unpack=True, delimiter='    ')
+
+fig = plt.figure(figsize=(12, 8))
+ax1 = fig.add_subplot(1, 1, 1)
+
+ax1.plot(x, y, '-')
+ax1.set_xlabel(r'$\xi_{new}$')
+ax1.set_ylabel('Free energy (kJ/mol)')
+
+id=0
+while os.path.exists("pmf.%s.jpeg" % id):
+    id+=1
+plt.savefig("pmf."+str(id)+".jpeg")
+plt.close()
