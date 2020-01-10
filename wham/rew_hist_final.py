e1afaa0d5b4f0b5e9c94741bb7385bffe16ffdd0
Jeremy Lapierre <jeremy.lapierre@uni-saarland.de>
2020-01-10 16:37:40 +0100

    reweighted histo more flexible (chose edges and step with flag) and able to do unweighted histo of any plumed column as well

--- wham/rew_hist_final.py
+++ wham/rew_hist_final.py
@@ -6,42 +6,61 @@ import pandas as pd
 import argparse
 import re
 import sys
+import os
 
 def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', help='give colvar_file, for building weithed histo', action='store', dest='f', required=True)        
-    parser.add_argument('--plot', help='plot histograms', action='store_true', dest='plot')
+    parser.add_argument('--plot', help='plot histograms together: all, or separately: sep', action='store', dest='plot')
     parser.add_argument('-k', help='force constant used for each windows', action='store', dest='k', required=True) 
+    parser.add_argument('-s', help='binning step, deflt=0.01 for ext -1.25 and 1.25', action='store', dest='s', default=0.01, type=float)
+    parser.add_argument('-min', help='min boundary for hist, deflt=-1.25', action='store', dest='min', default=-1.25, type=float)
+    parser.add_argument('-max', help='max boundary for hist, deflt=1.25', action='store', dest='max', default=1.25, type=float)
+    parser.add_argument('-rew', help='if present, allows to reweight histo by the value of the guide_restraint.bias', action='store_true', dest='rew')
+    parser.add_argument('-col', help='from which column of the colvar file do you want to do an histogram, deflt=nCV', default='nCV', action='store', dest='col', type=str)
+    parser.add_argument('--o', help='name of hist file', action='store', dest='o', type=str)
     args = parser.parse_args()
 
+    print('careful: put the -rew option added in the last maj for reweighting !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 
-    if not args.plot:
-        import os
+    if args.plot == None:
         pos=re.search('(-|[0-9]|\.)+(?=_)', args.f).group()    
         
         if os.path.exists('./histo_'+str(pos)+'_.dat'):
             print('./histo_'+str(pos)+'_.dat exists !')
             sys.exit()
+        elif os.path.exists(args.o):
+            print(args.o+' exists !')
+            sys.exit()
         else:
             df=plumed_pandas.read_as_pandas(args.f)
-            df = df[['nCV','guide_restraint.bias']]
-            
-            hist2, bin_edges = np.histogram(df['nCV'], bins=[i for i in np.around(np.arange(-1.25,1.25,0.01), decimals=3)], weights=[np.exp(i/2.49434) for i in df['guide_restraint.bias']],density=True)
+
+            if not args.rew:
+                df = df[args.col]
+                hist2, bin_edges = np.histogram(df, bins=[i for i in np.around(np.arange(args.min,args.max,args.s), decimals=3)],density=True)
+                hist2 = hist2 / hist2.sum()
+            else:
+                df = df[['nCV','guide_restraint.bias']]
+                hist2, bin_edges = np.histogram(df['nCV'], bins=[i for i in np.around(np.arange(args.min,args.max,args.s), decimals=3)], weights=[np.exp(i/2.49434) for i in df['guide_restraint.bias']],density=True)
             
             d = {'z': bin_edges[0:-1],'hist': hist2}
             
             hdf=pd.DataFrame(d)
             
             # /!\ saved in working directory
-            print('./histo_'+str(pos)+'_.dat is done')
-            np.savetxt(r'./histo_'+str(pos)+'_.dat', hdf.values, header="col1=z col2=hist\n#1 #2 "+str(pos)+"\n#1 #2 "+str(args.k), fmt='%.6f')
+            if args.o == None:
+                np.savetxt(r'./histo_'+str(pos)+'_.dat', hdf.values, header="col1=z col2=hist\n#1 #2 "+str(pos)+"\n#1 #2 "+str(args.k), fmt='%.6f')
+                print('./histo_'+str(pos)+'_.dat is done')
+            else:
+                np.savetxt(args.o, hdf.values, fmt='%.6f')
+                print(args.o+' is done')
 
-    if args.plot:
+    if args.plot == 'all':
 
         import matplotlib.pyplot as plt
-        import os       
         import matplotlib
+
         matplotlib.rcParams.update({'font.size': 23})
 
  
@@ -58,6 +77,27 @@ def main():
 
         plt.xlim(min(z), max(z))
         plt.savefig('hist.jpeg')
+
+    if args.plot == 'sep':
+  
+        import matplotlib.pyplot as plt
+        import matplotlib
+
+        histo_files = os.popen('ls histo* | sort -t _ -k 2 -n').read().split()
+
+        for h in histo_files:
+
+            fig, ax = plt.subplots(figsize=(15,8))
+
+            z, thishist = np.loadtxt(h, unpack=True)
+            Eg = h.split('_')[2]
+
+            ax.plot(z, thishist)
+            print(np.full((2,), float(Eg)), np.asarray([min(thishist), max(thishist)]), sep='\n')
+            ax.plot(np.full((2,), float(Eg)), np.asarray([min(thishist), max(thishist)]))
+            
+            plt.title(h.strip('.txt'))
+            plt.savefig(h.strip('.txt')+'.jpeg')
 
 # If called from the command line... 
 if __name__ == "__main__":
