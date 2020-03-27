d214d94b163b921ef186c1d35ea20f02d4fec81c
Jeremy <jeremy.lapierre@uni-saarland.de>
2020-03-27 22:45:35 +0100

    adding script given by Jochen to add TER flag in pdb

---
+++ correct-chainid-and-ter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+#script given by Jochen Hub
+
+import sys, string
+
+try:
+        f=open(sys.argv[1],"r")
+except:
+        print 'Usage:',sys.argv[0],'pdbfile'
+
+labels='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789'
+labels=labels+labels+labels+labels+labels+labels+labels+labels+labels
+
+last=-1
+chainnr=0
+
+for line in f:
+    if (not (line[0:6]=='ATOM  ' or line[0:6]=='HETATM')):
+        print line,
+    else:
+        res     = int(line[22:26])
+        # chainID = labels[chainnr]
+        
+        if (res<last):
+            print 'TER'
+            chainnr += 1
+        last = res
+
+        line = line[0:21] + labels[chainnr] + line[22:]
+        print line,
