1fa7b95465daae6ad629d320ec4cdc0d8db662a2
Jeremy Lapierre <jeremy.lapierre@uni-saarland.de>
2020-08-10 12:17:31 +0200

    adding script for plotting torcv results

---
+++ plot_tor.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+'''plotting output of torcv tries'''
+
+import argparse
+import matplotlib.pyplot as plt
+import plumed_pandas
+
+def main():
+    '''plotting output of torcv tries'''
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='give colvar_file', action='store', dest='f', type=str)
+    parser.add_argument('-start', help='give start position of column', action='store',
+                        dest='start', type=int)
+    parser.add_argument('-end', help='give end position +1 of column', action='store',
+                        dest='end', type=int)
+    parser.add_argument('-col', help='list of column names to plot', nargs='*', dest='col')
+    parser.add_argument('-o', help='output image name / path', action='store', dest='out',
+                        type=str)
+    args = parser.parse_args()
+
+    #def skip(index):
+    #    if index % 100 == 0:
+    #        return True
+    #    return False
+
+    dataf = plumed_pandas.read_as_pandas(args.f, skiprows=lambda x: x % 100)
+
+    fig = plt.figure(figsize=[8, 8])
+    axs = fig.gca()
+    if args.col is not None:
+        dataf.plot(x='time', y=args.col, ax=axs)
+    else:
+        dataf.plot(x='time', y=range(args.start, args.end), ax=axs)
+    plt.savefig(f'{args.out}.png', format='png')
+    plt.close()
+    print(f'{args.out}.png has been created')
+
+if __name__ == '__main__':
+    main()
