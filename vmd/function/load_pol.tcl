eec25da00430a48ee64a80c806f29fa658283d96
Jeremy Lapierre <jeremy.lapirre@uni-saarland.de>
2020-12-09 10:36:34 +0100

    minor changes

--- vmd/function/load_pol.tcl
+++ vmd/function/load_pol.tcl
@@ -10,7 +10,7 @@ proc load_pol {args} {
     set mol 0
     foreach x $args {
         puts "loading $x/0.pdb $x/nopbc2.xtc"
-	mol new $x/0.pdb type {pdb} first 0 last -1 step 1 waitfor all
+	mol new $x/0_chains.pdb type {pdb} first 0 last -1 step 1 waitfor all
         mol addfile $x/nopbc2.xtc type {xtc} first 1 last -1 step 1 molid $mol waitfor all
 	mol rename $mol $x
         mol selection nucleic
