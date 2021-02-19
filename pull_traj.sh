083f084d65a0a4213d4ef47be447728a89c34916
jerem2401 <jeremy.lapierre@uni-saarland.de>
2021-02-19 14:41:43 +0100

    command line options for pull_traj

--- pull_traj.sh
+++ pull_traj.sh
@@ -1,46 +1,68 @@
 #! /bin/bash
 
-#for i in rep*/*/; do
-#    echo $i
-#    echo 1 | gmx trjconv -f $i/traj_comp.xtc -s $i/md.tpr -o $i/nopbc1.xtc -pbc atom -ur compact -dt 600
-#    wait $!
-#    echo 1 | gmx trjconv -f $i/nopbc1.xtc -s $i/md.tpr -o $i/nopbc2.xtc -pbc whole
-#    wait $!
-#    echo 1 | gmx trjconv -f $i/nopbc2.xtc -s $i/md.tpr -dump 0 -o $i/0.gro
-#done
-echo $1
+PWD=$(pwd)
 
-if [ -z "$2" ]; then
+if [[ "$PWD" =~ .*t4.* ]]; then
+	i=$1
+	echo $i
+	echo 1 | gmx trjconv -f $i/traj_comp.xtc -s $i/md.tpr -o $i/nopbc1.xtc -pbc atom -ur compact
+	wait $!
+	echo 1 | gmx trjconv -f $i/nopbc1.xtc -s $i/md.tpr -o $i/nopbc2.xtc -pbc whole
+	wait $!
+	echo 1 | gmx trjconv -f $i/nopbc2.xtc -s $i/md.tpr -dump 0 -o $i/0.gro
+fi
+
+
+if [[ "$PWD" =~ .*pol.* ]]; then
+
 	skip=4
-else
-	skip=$2
+	group=26
+	index="$HOME/simulation/syncsim/pol/ref/cc_ZN_noTFIIS_params/prep/nvt/index.ndx"
+	proto=1
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+			-d)
+			  shift
+			  dir=$1
+			  ;;
+			-skip)
+			  shift
+			  skip=$1
+			  ;;
+			-group)
+			  shift
+			  group=$1
+			  ;;
+			-protocol)
+			  shift
+			  proto=$1
+			  ;;
+		esac
+		shift
+	done
+	echo "dir is: $dir, skip is: $skip, group is: $group, protocol is: $proto"
+
+	if [ "$dir" == '' ]; then
+		echo "no dir given, exiting"
+		exit
+	fi
+
+	if (("$proto" = 1)); then
+		echo $group | gmx trjconv -f $dir/traj_comp.xtc -s $dir/md.tpr -o $dir/nopbc1.xtc -ur compact -pbc atom -n $index -skip $skip
+		wait
+		echo $group | gmx trjconv -f $dir/nopbc1.xtc -s $dir/md.tpr -o $dir/nopbc2.xtc -ur compact -pbc whole -n $index
+		wait
+	else
+		echo $group | gmx trjconv -f $dir/traj_comp.xtc -s $dir/md.tpr -o $dir/nopbc1.xtc -ur compact -pbc mol -n $index -skip $skip
+		wait
+		echo $group | gmx trjconv -f $dir/nopbc1.xtc -s $dir/md.tpr -o $dir/nopbc2.xtc -pbc nojump -n $index
+		wait
+	fi
+
+	module load conda
+	source activate env1
+
+	echo $group | gmx trjconv -f $dir/nopbc2.xtc -s $dir/md.tpr -o $dir/0.pdb -dump 0 -n $index
+	correct-chainid-and-ter.py $dir/0.pdb > $dir/0_chains.pdb
 fi
-
-echo $2
-
-if [ -z "$3" ]; then
-	group=24
-else
-	group=$3
-fi
-
-echo $3
-
-read -p "which index.ndx, 0 (old) or 1(new) ?" ival
-
-if (( "$ival" == 0 )); then
-	index="/data/users/jeremy/simulation/syncsim/pol/meta/index.ndx"
-elif (( "$ival" == 1 )); then
-	index="/data/users/jeremy/simulation/syncsim/pol/ref/cc_ZN_noTFIIS/prep/npt/index.ndx"
-else
-	echo "error: no index file choosen"
-	exit 1
-fi
-
-echo $group | gmx trjconv -f $1/traj_comp.xtc -s $1/md.tpr -o $1/nopbc1.xtc -ur compact -pbc atom -n $index -skip $skip
-wait
-echo $group | gmx trjconv -f $1/nopbc1.xtc -s $1/md.tpr -o $1/nopbc2.xtc -ur compact -pbc whole -n $index
-wait
-echo $group | gmx trjconv -f $1/nopbc2.xtc -s $1/md.tpr -o $1/0.pdb -dump 0 -n $index
-
-correct-chainid-and-ter.py $1/0.pdb > $1/0_chains.pdb
