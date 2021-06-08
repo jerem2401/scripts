1c3c1851ccb630c0b910adfefcede31b534115fc
jerem2401 <jeremy.lapierre@uni-saarland.de>
2021-06-08 21:37:08 +0200

    hot fixes

--- hbonds/statssr.py
+++ hbonds/statssr.py
@@ -12,26 +12,41 @@ from uncertainties import ufloat
 
 def bywin(tdir):
     out = open(f"{tdir}/analyzehb.txt", 'a')
-    out.write(f"[ Prot-DNA nb SR interactions ]\n")
-    out.write(f"#coul lj\n")
 
-    xvgs = glob.glob(f"{tdir}/outnrg.txt")
+    xvgs = glob.glob(f"{tdir}/outnrg*.txt")
     xvgs = sorted(xvgs, key=lambda x: x.split('/')[-2].split('_')[-1])
 
     for xvg in xvgs:
         with open(xvg, 'r') as reader:
             for line in reader:
                 if "Coul-SR:Protein-DNA" in line and 'LJ-SR:Protein-DNA' not in line:
+                    tag = 'Protein-DNA'
                     mean_coul = float(line.split()[1])
                     err_coul = float(line.split()[2])
-                elif "LJ-SR:Protein-DN" in line and 'Coul-SR:Protein-DNA' not in line:
+                elif "LJ-SR:Protein-DNA" in line and 'Coul-SR:Protein-DNA' not in line:
+                    mean_lj = float(line.split()[1])
+                    err_lj = float(line.split()[2])
+                elif "Coul-SR:DNA-DNA" in line and 'LJ-SR:DNA-DNA' not in line:
+                    tag = 'DNA-DNA'
+                    mean_coul = float(line.split()[1])
+                    err_coul = float(line.split()[2])
+                elif "LJ-SR:DNA-DNA" in line and 'Coul-SR:DNA-DNA' not in line:
+                    mean_lj = float(line.split()[1])
+                    err_lj = float(line.split()[2])
+                if "Coul-SR:DNA-Water" in line and 'LJ-SR:DNA-Water' not in line:
+                    tag = 'DNA-Water'
+                    mean_coul = float(line.split()[1])
+                    err_coul = float(line.split()[2])
+                elif "LJ-SR:DNA-Water" in line and 'Coul-SR:DNA-Water' not in line:
                     mean_lj = float(line.split()[1])
                     err_lj = float(line.split()[2])
 
+        out.write(f"[ {tag} nb SR interactions ]\n")
+        out.write(f"#coul lj\n")
         coul = ufloat(mean_coul, err_coul)
         lj = ufloat(mean_lj, err_lj)
         out.write(f"{coul}     {lj}\n")
-        out.write(f"\n[ Prot-DNA Coul+LJ ]\n")
+        out.write(f"\n[ {tag} Coul+LJ ]\n")
         sr_sum = coul+lj
         out.write(f"{sr_sum}\n")
 
