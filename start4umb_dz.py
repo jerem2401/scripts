a1ecdb4ed21dcf0e19663ced5e014e8a98e77702
jerem2401 <jeremy.lapierre@uni-saarland.de>
2021-12-09 18:43:19 +0100

    adding start4umb_dz.py umbsetup_dz.sh for US on Z, think about merging all the version to make one general scripts

---
+++ start4umb_dz.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import plumed_pandas
+
+def main(colvf, ksi):
+    '''
+    parsing the colvar.txt, find the closest nCV value to target_val
+    '''
+
+    #read COLVAR.txt
+    df = plumed_pandas.read_as_pandas(colvf)
+
+    mindiff = (df['dz']-float(ksi)).abs().idxmin()
+    found = df.iloc[mindiff]['dz']
+    tvalue = df.iloc[mindiff]['time']
+
+    return found, tvalue
+
+# If called from the command line...
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', help='give colvar_files', action='store', dest='f', type=str)
+    parser.add_argument('-v', help='tnCV', action='store', dest='v', type=float)
+    args = parser.parse_args()
+
+    F, TV = main(args.f, args.v)
+    print(F, TV)
