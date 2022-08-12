baab81ab87787bfa272fed6bacee74ad38e8ba69
JEremy <jeremy.lapirre@uni-saarland.de>
2022-08-12 17:27:57 +0200

    minor fixes

--- hbonds/mk_bp_ndxP.sh
+++ hbonds/mk_bp_ndxP.sh
@@ -41,78 +41,78 @@ done
 
 if (($mknd == 1)); then
 
-	[[ -d $dir ]] && echo "$dir already exists, exiting" && exit || mkdir $dir
-	if ls $dir/DNA* &> /dev/null; then echo "DNAs dir exist"; exit; fi
+	#[[ -d $dir ]] && echo "$dir already exists, exiting" && exit || mkdir $dir
+	#if ls $dir/DNA* &> /dev/null; then echo "DNAs dir exist"; exit; fi
 
-	while read line; do
-		f=$(echo $line | awk '{print $1}')
-		fn=$(echo $line | awk '{print $2}')
-		fa3=''
-		sa3=''
-		if [ $fn == "DC" ]; then
-			fa1="atomname N3"
-			fa2="atomname O2"
-			fa3="(atomname N4 or atomname H41 or atomname H42)"
-			sa1="(atomname N1 or atomname H1)"
-			sa2="(atomname N2 or atomname H21 or atomname H22)"
-			sa3="atomname O6"
-		elif [ $fn == "DG" ]; then
-			fa1="(atomname N1 or atomname H1)"
-			fa2="(atomname N2 or atomname H21 or atomname H22)"
-			fa3="atomname O6"
-			sa1="atomname N3"
-			sa2="atomname O2"
-			sa3="(atomname N4 or atomname H41 or atomname H42)"
-		elif [ $fn == "DA" ]; then
-			fa1="(atomname N6 or atomname H61 or atomname H62)"
-			fa2="atomname N1"
-			sa1="atomname O4"
-			sa2="(atomname N3 or atomname H3)"
-		elif [ $fn == "DT" ]; then
-			fa1="atomname O4"
-			fa2="(atomname N3 or atomname H3)"
-			sa1="(atomname N6 or atomname H61 or atomname H62)"
-			sa2="atomname N1"
-		else
-			echo "error, exiting"
-			exit
-		fi
-		s=$(echo $line | awk '{print $3}')
-		#sn=$(echo $line | awk '{print $4}')
-
-
-		if [[ ! -z $fa3 ]]; then
-			mkdir "${dir}/DNAt_${f}"
-			sel=$(echo "group \"Protein\" and within \
-			0.4 of (group \"DNAt\" and resid $f and ($fa1 or $fa2 or $fa3));")
-			echo $sel > "${dir}/DNAt_${f}/selection.dat"
-			gmx select -sf "${dir}/DNAt_${f}/selection.dat" -f ${dirumb}/umb2.xtc \
-			-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAt_${f}/hbindex.ndx
-
-			mkdir "${dir}/DNAnt_${s}"
-			sel=$(echo "group \"Protein\" and within \
-			0.4 of (group \"DNAnt\" and resid $s and ($sa1 or $sa2 or $sa3));")
-			echo $sel > "${dir}/DNAnt_${s}/selection.dat"
-			gmx select -sf "${dir}/DNAnt_${s}/selection.dat" -f ${dirumb}/umb2.xtc \
-			-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAnt_${s}/hbindex.ndx
-		else
-			mkdir "${dir}/DNAt_${f}"
-			sel=$(echo "group \"Protein\" and within \
-			0.4 of (group \"DNAt\" and resid $f and ($fa1 or $fa2));")
-			echo $sel > "${dir}/DNAt_${f}/selection.dat"
-			gmx select -sf "${dir}/DNAt_${f}/selection.dat" -f ${dirumb}/umb2.xtc \
-			-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAt_${f}/hbindex.ndx
-
-			mkdir "${dir}/DNAnt_${s}"
-			sel=$(echo "group \"Protein\" and within \
-			0.4 of (group \"DNAnt\" and resid $s and ($sa1 or $sa2));")
-			echo $sel > "${dir}/DNAnt_${s}/selection.dat"
-			gmx select -sf "${dir}/DNAnt_${s}/selection.dat" -f ${dirumb}/umb2.xtc \
-			-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAnt_${s}/hbindex.ndx
-		fi
+	#while read line; do
+	#	f=$(echo $line | awk '{print $1}')
+	#	fn=$(echo $line | awk '{print $2}')
+	#	fa3=''
+	#	sa3=''
+	#	if [ $fn == "DC" ]; then
+	#		fa1="atomname N3"
+	#		fa2="atomname O2"
+	#		fa3="(atomname N4 or atomname H41 or atomname H42)"
+	#		sa1="(atomname N1 or atomname H1)"
+	#		sa2="(atomname N2 or atomname H21 or atomname H22)"
+	#		sa3="atomname O6"
+	#	elif [ $fn == "DG" ]; then
+	#		fa1="(atomname N1 or atomname H1)"
+	#		fa2="(atomname N2 or atomname H21 or atomname H22)"
+	#		fa3="atomname O6"
+	#		sa1="atomname N3"
+	#		sa2="atomname O2"
+	#		sa3="(atomname N4 or atomname H41 or atomname H42)"
+	#	elif [ $fn == "DA" ]; then
+	#		fa1="(atomname N6 or atomname H61 or atomname H62)"
+	#		fa2="atomname N1"
+	#		sa1="atomname O4"
+	#		sa2="(atomname N3 or atomname H3)"
+	#	elif [ $fn == "DT" ]; then
+	#		fa1="atomname O4"
+	#		fa2="(atomname N3 or atomname H3)"
+	#		sa1="(atomname N6 or atomname H61 or atomname H62)"
+	#		sa2="atomname N1"
+	#	else
+	#		echo "error, exiting"
+	#		exit
+	#	fi
+	#	s=$(echo $line | awk '{print $3}')
+	#	#sn=$(echo $line | awk '{print $4}')
 
 
-	done < $base/simulation/syncsim/pol/heavy_h/ref/pairs2P.txt
+	#	if [[ ! -z $fa3 ]]; then
+	#		mkdir "${dir}/DNAt_${f}"
+	#		sel=$(echo "group \"Protein\" and within \
+	#		0.4 of (group \"DNAt\" and resid $f and ($fa1 or $fa2 or $fa3));")
+	#		echo $sel > "${dir}/DNAt_${f}/selection.dat"
+	#		gmx select -sf "${dir}/DNAt_${f}/selection.dat" -f ${dirumb}/umb2.xtc \
+	#		-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAt_${f}/hbindex.ndx
+
+	#		mkdir "${dir}/DNAnt_${s}"
+	#		sel=$(echo "group \"Protein\" and within \
+	#		0.4 of (group \"DNAnt\" and resid $s and ($sa1 or $sa2 or $sa3));")
+	#		echo $sel > "${dir}/DNAnt_${s}/selection.dat"
+	#		gmx select -sf "${dir}/DNAnt_${s}/selection.dat" -f ${dirumb}/umb2.xtc \
+	#		-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAnt_${s}/hbindex.ndx
+	#	else
+	#		mkdir "${dir}/DNAt_${f}"
+	#		sel=$(echo "group \"Protein\" and within \
+	#		0.4 of (group \"DNAt\" and resid $f and ($fa1 or $fa2));")
+	#		echo $sel > "${dir}/DNAt_${f}/selection.dat"
+	#		gmx select -sf "${dir}/DNAt_${f}/selection.dat" -f ${dirumb}/umb2.xtc \
+	#		-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAt_${f}/hbindex.ndx
+
+	#		mkdir "${dir}/DNAnt_${s}"
+	#		sel=$(echo "group \"Protein\" and within \
+	#		0.4 of (group \"DNAnt\" and resid $s and ($sa1 or $sa2));")
+	#		echo $sel > "${dir}/DNAnt_${s}/selection.dat"
+	#		gmx select -sf "${dir}/DNAnt_${s}/selection.dat" -f ${dirumb}/umb2.xtc \
+	#		-s ${dirumb}/subset.tpr -n $index -on ${dir}/DNAnt_${s}/hbindex.ndx
+	#	fi
+
+
+	#done < $base/simulation/syncsim/pol/heavy_h/ref/pairs2P.txt
 
 	for i in ${dir}/DNA*; do
 		atominrange=$(egrep -c -v '(\[|^$)' $i/hbindex.ndx)
@@ -127,6 +127,7 @@ if (($mknd == 1)); then
 			echo "$pgrp;" > "${i}/merge_sel.dat"
 			gmx select -sf "${i}/merge_sel.dat" -f ${dirumb}/umb2.xtc \
 			-s ${dirumb}/subset.tpr -n $i/hbindex.ndx -on ${i}/hbindexm.ndx
+			sed '1 s/^.*$/\[ my_custom_group_along_traj \]/' hbindexm.ndx
 
 			grep -oP '(?<=of \().*(?=\);)' $i/selection.dat > ${i}/selectionnuc.dat
 			gmx select -sf ${i}/selectionnuc.dat -f ${dirumb}/umb2.xtc \
